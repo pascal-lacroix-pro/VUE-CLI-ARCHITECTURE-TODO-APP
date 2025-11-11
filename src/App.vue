59e3d1842c763fc5d61739e11dbfa46dcb7fdeba
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-11-11 22:55:38 +0100

    Installation Tailwind CSS 4

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="text-3xl">You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
